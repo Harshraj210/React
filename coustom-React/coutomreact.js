03232d3f769e7596501d348eac2c998446cc70bd
harsh <nh1750501@gmail.com>
2025-08-23 20:30:15 +0530

    Added custom React file

---
+++ coustom-React/coutomreact.js
@@ -0,0 +1,14 @@
+function mainConatiner(Reactelement,container){
+  document.createElement(Reactelement.type)
+}
+const Reactelement={
+type:'a',
+  props:{
+    href:https://goggle.com,
+    target:'_blank'
+  },
+  Children:"CLick me to visit Google :)"
+
+}
+const mainContainer=document.querySelector("#root")
+customrender(Reactelement,mainContainer)
