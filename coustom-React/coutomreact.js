ca7c17bc67e290441c51ffa5f095e60dee740422
harsh <nh1750501@gmail.com>
2025-08-23 20:51:39 +0530

    updated code base

--- coustom-React/coutomreact.js
+++ coustom-React/coutomreact.js
@@ -1,10 +1,25 @@
-function mainConatiner(Reactelement,container){
-  document.createElement(Reactelement.type)
-}
+function customrender(Reactelement,container){
+  // const domElement=document.createElement(Reactelement.type)
+  // domElement.innerHTML=Reactelement.Children
+  // domElement.setAttribute('href',Reactelement.props.href)
+  // domElement.setAttribute('target',Reactelement.props.target)
+  // container.appendChild(domElement)
+
+  const domElement=document.createElement(Reactelement.type)
+  domElement.innerHTML=Reactelement.Children
+  for (const prop in Reactelement.props) {
+    if(prop === 'children')continue
+    // not return bcz it will close entire thing
+    domElement.setAttribute(prop,Reactelement.props[prop])
+
+    }
+  }
+  
+
 const Reactelement={
 type:'a',
   props:{
-    href:https://goggle.com,
+    href:"https://goggle.com",
     target:'_blank'
   },
   Children:"CLick me to visit Google :)"
