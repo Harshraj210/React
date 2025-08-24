45abcc4458718a1b284a3115c72364a85bd12621
harsh <nh1750501@gmail.com>
2025-08-24 20:12:04 +0530

    updated code base

--- 02_react/src/App.jsx
+++ 02_react/src/App.jsx
@@ -3,25 +3,26 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  // to update value on webpage 
+  // to update value on webpage
+  // usestate--> hooks helps you add variable which rerenders  the UI
 
-  const [counter,setCounter] = useState(19)
+  
+  const [counter, setCounter] = useState(19);
   // [variable , method]
   // let counter = 10;
-  const addValue = ()=>{
-    setCounter(counter+4)
-    
-  }
-  const removeValue=()=>{
-    setCounter(counter-1)
-  }
+  const addValue = () => {
+    setCounter(counter + 1);
+  };
+  const removeValue = () => {
+    setCounter(counter - 1);
+  };
   return (
     <>
-      <h1>Welcome to React</h1>
+      <h1>Welcome to React Counter</h1>
       {/* variable injection */}
 
       <h2>Counter value : {counter}</h2>
-      <button onClick={addValue}>Add Value</button> 
+      <button onClick={addValue}>Add Value</button>
       <button onClick={removeValue}>Remove Value</button>
       <p>Footer :</p>
     </>
