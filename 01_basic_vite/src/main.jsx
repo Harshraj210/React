e39dfec411f6ff0a54d3373aa004a8ecf22cdf28
harsh <nh1750501@gmail.com>
2025-08-24 13:53:47 +0530

    updated code base

--- 01_basic_vite/src/main.jsx
+++ 01_basic_vite/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+const Reactelement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  Children: "CLick me to visit Google :)",
+};
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function Myapp(){
+  return(
+    <div>
+      <h1>hello from custom react</h1>
+    </div>
+  )
+}
+
+const AnotherElement=(
+  <a href ="https://google.com" target="_blank">Visit Google</a>
+  // it takes only evaluated expression not which has to be eveluated
 )
+
+createRoot(document.getElementById("root")).render(
+  AnotherElement()
+);
--- coustom-React/coutomreact.js
+++ coustom-React/coutomreact.js
@@ -1,30 +1,28 @@
-function customrender(Reactelement,container){
+function customrender(Reactelement, container) {
   // const domElement=document.createElement(Reactelement.type)
   // domElement.innerHTML=Reactelement.Children
   // domElement.setAttribute('href',Reactelement.props.href)
   // domElement.setAttribute('target',Reactelement.props.target)
   // container.appendChild(domElement)
 
-  const domElement=document.createElement(Reactelement.type)
-  domElement.innerHTML=Reactelement.Children
+  const domElement = document.createElement(Reactelement.type);
+  domElement.innerHTML = Reactelement.Children;
   for (const prop in Reactelement.props) {
-    if(prop === 'Children')continue
+    if (prop === "Children") continue;
     // not return bcz it will close entire thing
-    domElement.setAttribute(prop,Reactelement.props[prop])
-
-    }
-    container.appendChild(domElement)
+    domElement.setAttribute(prop, Reactelement.props[prop]);
   }
-  
-
-const Reactelement={
-type:'a',
-  props:{
-    href:"https://google.com",
-    target:'_blank'
-  },
-  Children:"CLick me to visit Google :)"
-
+  container.appendChild(domElement);
 }
-const mainContainer=document.querySelector("#root")
-customrender(Reactelement,mainContainer)
+
+const Reactelement = {
+  // <a-tag>
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  Children: "CLick me to visit Google :)",
+};
+const mainContainer = document.querySelector("#root");
+customrender(Reactelement, mainContainer);
