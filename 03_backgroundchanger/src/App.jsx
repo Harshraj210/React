1b4817fca267a0e0fc0071b3f4ea7a5bc98f5f0f
harsh <nh1750501@gmail.com>
2025-08-25 23:52:02 +0530

    made a background changer

--- 03_backgroundchanger/src/App.jsx
+++ 03_backgroundchanger/src/App.jsx
@@ -1,23 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive");
+
+  function changeColor(newColor) {
+    setColor((prev) => (prev === "olive" ? "red" : "olive"));
+  }
 
   return (
-    <>
-      <div className='w-full h-screen duration-300'>
-        <div className='fixed flex flex-wrap justify-center bottom-16 inset-x-0 x px-2'>
-          <div className='fixed flex flex-wrap justify-center w-full shadow-lg bg-white px-3 py-2 rounded-3xl'>
+    <div
+      className="w-full h-screen transition-colors duration-300"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed bottom-16 inset-x-0 flex justify-center px-2">
+        <div className="flex justify-center w-full shadow-lg px-3 py-2 rounded-3xl bg-white">
+          <button
+            onClick={changeColor}
+              
             
-
-          </div>
+            className="px-6 py-2 rounded-full font-semibold text-white bg-blue-500 shadow-md hover:bg-blue-600 active:scale-95 transition duration-200"
+          >
+            Change Background
+          </button>
         </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
