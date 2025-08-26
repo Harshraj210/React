22a2334b3a4721734ece2c5080e9559a94dd0b17
harsh <nh1750501@gmail.com>
2025-08-26 13:43:38 +0530

    updated code bse

--- 04_passwordgen/src/App.jsx
+++ 04_passwordgen/src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [length,setlength] = useState(8)
-  const [numberAllowed,setnumberAllowed]=useState(flase)
-  const [charAllowed,setcharAllowed]=useState(flase)
-  const [password,setpassword]=useState('')
+  const [length, setlength] = useState(8);
+  const [numberAllowed, setnumberAllowed] = useState(false);
+  const [charAllowed, setcharAllowed] = useState(false);
+  const [password, setpassword] = useState("");
   return (
     <>
-     <h1 className='bg-red-500'>Set-up rea  dy</h1>
-    </> 
-  )
+      <div className="w-full max-w-lg mx-auto shadow-xl rounded-lg p-6 my-10 bg-gray-800 text-orange-500">
+        <h1 className="text-white text-center text-2xl mb-4">
+          Password Generator
+        </h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-2 px-4 bg-gray-700 text-white"
+            placeholder="Generated Password"
+            readOnly
+          />
+          <button className="outline-none bg-blue-600 text-white px-4 py-1 shrink-0 hover:bg-blue-700 active:scale-95 transition-transform duration-150">
+            Copy
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
