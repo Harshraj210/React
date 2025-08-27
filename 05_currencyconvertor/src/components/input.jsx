395e5e54272f1ee9ecd0d3df7b2d41ac0dc033bf
harsh <nh1750501@gmail.com>
2025-08-27 13:07:04 +0530

    updated code base

--- 05_currencyconvertor/src/components/input.jsx
+++ 05_currencyconvertor/src/components/input.jsx
@@ -5,9 +5,44 @@ function input() {
     onammountchange,
     oncurrencychange,
     (currencyoptions = []),
-    (selectedcurrency = "usd");
+    (selectedcurrency = "usd"),
+    (currencydisabled = false),
+    (className = "");
 
-  return <div>input</div>;
+  return (
+    <div className={`bg-white p-3rounded-lg text-sm flex ${className}`}>
+      Input box
+      <div className="w-1-2">
+        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <input
+          text="number"
+          className="outline-none w-full bg-transparent py-1.5 text-lg"
+          placeholder="Amount"
+          disabled={amountDisabled}
+          value={amount}
+          onChange={(e) =>
+            onammountchange && onammountchange(Number(e.target.value))
+          }
+          // without number it takes string also
+        ></input>
+      </div>
+      <div className="w-1/2 flex flex-col items-end text-right">
+          <p className="text-black/40 mb-2 w-full">Currency Type</p>
+          <select 
+            className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+            value={selectedcurrency}
+            onChange={oncurrencychange && oncurrencychange(Number(e.target.value))}
+            disabled={currencydisabled}
+          
+            >
+              {currencyoptions.map((Currency)=>(
+                // no {} as wedon't need to return    
+                <option key={key} value={Currency}>{Currency}</option>
+              ))}
+            </select>
+        </div>
+    </div>
+  );
 }
 
 export default input;
