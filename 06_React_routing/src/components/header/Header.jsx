e5f7a4422ac2a91cfb49b58c2d373daaa257df65
harsh <nh1750501@gmail.com>
2025-08-28 15:55:27 +0530

    updated code base

--- 06_React_routing/src/components/header/Header.jsx
+++ 06_React_routing/src/components/header/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className='shadow sticky z-50 top-0'>
+      <nav className='bg-white border-gray-500 px-4 lg:px-6 py-2.5'></nav>
+    </header>
   )
 }
 
