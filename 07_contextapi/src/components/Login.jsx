d1f5dab8fdb6edf471e7f49b719cf078100f6763
harsh <nh1750501@gmail.com>
2025-08-30 14:44:28 +0530

    Updated code base

---
+++ 07_contextapi/src/components/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function Login(){
+  const [Username,setUsername]=useState('')
+  const [Password,setPassword]=useState('')
+
+  // prevents from Refreshing
+
+  const handleSubmit=(e)=>{
+
+  }
+  return (
+    <div>
+      <h2>LogIn</h2>
+      <input 
+      type="text"
+      value={Username}
+      onChange={(e)=>setUsername(e.target.value)} 
+      placeholder="username"
+      />
+      {" "}
+      <input 
+      type="password"
+      value={Password}
+      onChange={(e)=>setPassword(e.target.value)} 
+      placeholder="Password"
+      />
+      <button
+      onClick={handleSubmit}>
+        LogIn
+      </button>
+    </div>
+  )
+}
+
+export default Login
